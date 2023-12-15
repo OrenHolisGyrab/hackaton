2790ebcbb75197cc564d272ae58a3bf64c6926fd
Voplakal <82812912+Voplakal@users.noreply.github.com>
2023-12-15 14:17:01 +0100

    .

--- fe/src/views/vypujcky/Aktivni.vue
+++ fe/src/views/vypujcky/Aktivni.vue
@@ -11,7 +11,7 @@
                 </h4>
                 <div class="small text-body-secondary">January 2021</div>
               </CCol>
-              <CCol :sm="7" class="d-none d-md-block">
+              <CCol class="">
                 <CButton color="primary" class="float-end" size="lg" @click="pujcit()">
                   Vypůjčit
                 </CButton>
@@ -38,38 +38,42 @@
     <CModalHeader>
       <CModalTitle>Přidat položku</CModalTitle>
     </CModalHeader>
-    <CModalBody>
-      item_code: 'DHM-2002',
-      first_name: 'Mark',
-      last_name: " Nopeeee",
-      email: 'mmm@asd.cz',
-      from: '3.3.2000',
-      to: '3.5.5000',
-      item_name: 'Name',
-      note: 'Poznamkskkaksk',
-      prolonged: true,
+    <CForm onsubmit="save">
+      <CModalBody>
+        <CFormInput v-model="novaVypujcka.item_code" list="datalistOptions" type="search" label="Kód položky" placeholder="vyhledat" text="todo: skenovat" required />
+        <datalist id="datalistOptions">
+          <option v-for="item in freeItems" :value="item.code">
+          </option>
+          <option value="San Francisco"></option>
+          <option value="New York"></option>
+          <option value="Seattle"></option>
+          <option value="Los Angeles"></option>
+          <option value="Chicago"></option>
+        </datalist>
+        <CFormInput v-model="novaVypujcka.email" type="email" label="Student" placeholder="email studenta" required />
 
-      <CForm>
-        <CFormInput type="text" label="Kód položky" placeholder="codeplaceholder" text="todo: skenovat" />
-        <CFormInput type="email" label="Student" placeholder="email studenta" />
-
-        <CFormInput type="date" label="Datum vypůjčení" placeholder=""/>
-        <CFormInput type="date" label="Datum vypůjčení" placeholder=""/>
-      </CForm>
-
-    </CModalBody>
+        <CFormInput v-model="novaVypujcka.from" type="date" label="Datum vypůjčení" required />
+        <CFormInput v-model="novaVypujcka.to" type="date" label="Datum navrácení" required />
+        <CFormTextarea v-model="novaVypujcka.note" label="Poznámka" rows="3" required></CFormTextarea>
+      </CModalBody>
+    </CForm>
     <CModalFooter>
       <CButton color="secondary" @click="() => { vypujcitModal = false }">
         Storno
       </CButton>
-      <CButton color="primary">Uložit</CButton>
+      <CButton color="primary" type="submit">Uložit</CButton>
     </CModalFooter>
   </CModal>
 </template>
-
+<style>
+label {
+  margin-top: 10px;
+}
+</style>
 <script>
-import { ref } from "vue";
+import { ref, reactive, computed } from "vue";
 import VypujckyTable from "./VypujckyTable";
+import { REST } from "../../utils/REST";
 export default {
   name: 'Aktivni',
   components: {
@@ -97,9 +101,23 @@ export default {
     const vypujcitModal = ref(false);
     const pujcit = () => {
       vypujcitModal.value = true;
+      loadItems()
     }
+
+    const novaVypujcka = reactive({});
+    const save = () => {
+      console.log("save");
+      console.log(novaVypujcka);
+    }
+
+    const freeItems = ref([]);
+    let loadItems = async () => {
+      freeItems.value = await REST.GET("lending/free/items");
+      console.log(freeItems.value);
+    }
+
     return {
-      vypujcky, pujcit, vypujcitModal
+      vypujcky, pujcit, vypujcitModal, novaVypujcka, save, freeItems
     }
   },
 }
