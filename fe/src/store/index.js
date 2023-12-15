09405823a5380a3eb190671e10c773a03ac75de7
oren.holis <oren@ilico.io>
2023-12-15 09:52:21 +0100

    Store init

--- fe/src/store/index.js
+++ fe/src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore } from 'vuex'
+import {REST} from "../utils/REST";
+import {Uploads} from "../utils/utils";
 
 export default createStore({
   state: {
@@ -16,7 +18,66 @@ export default createStore({
     updateSidebarVisible(state, payload) {
       state.sidebarVisible = payload.value
     },
+    setItems(state, data) {
+      state.items.items = data;
+    },
+    addItem(state, data) {
+      state.items.items.push(data);
+    },
+    updateItem(state, data) {
+      state.data.items.updateByIndex(data, i => i.id === data.id);
+    },
+    deleteItem(state, data) {
+      state.data.items.deleteByIndex(data, i => i.id === data.id);
+    },
+    importItems(state, data) {
+      state.data.items = state.data.items.concat(data);
+    },
+    setItemsError(state, data) {
+      state.data = data
+    },
   },
-  actions: {},
+  actions: {
+    async getItems(context) {
+      try {
+        const response = await REST.GET('items');
+        context.setItems(response);
+      } catch (error) {
+        context.setItemsError(error);
+      }
+    },
+    async newItem(context, data) {
+      try {
+        const response = await REST.POST('items', data);
+        context.addItem(response);
+      } catch (error) {
+        context.setItemsError(error);
+      }
+    },
+    async updateItem(context, {id, data}) {
+      try {
+        const response = await REST.POST(`items/${id}`, data);
+        context.updateItem(response);
+      } catch (error) {
+        context.setItemsError(error);
+      }
+    },
+    async deleteItem(context, id) {
+      try {
+        const response = await REST.DELETE(`items/${id}`);
+        context.deleteItem(response);
+      } catch (error) {
+        context.setItemsError(error);
+      }
+    },
+    async importItems(context, file) {
+      try {
+        const response = await Uploads.uploadFile(`items/import`, file);
+        context.importItems(response);
+      } catch (error) {
+        context.setItemsError(error);
+      }
+    }
+  },
   modules: {},
 })
