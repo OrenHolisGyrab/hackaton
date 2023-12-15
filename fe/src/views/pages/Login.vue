71cfe85616e68ae623b6d5d1fe249e1c92ac09e0
DenisOSVR <denis.masek@student.gyarab.cz>
2023-12-15 13:28:34 +0100

    Google button moved

--- fe/src/views/pages/Login.vue
+++ fe/src/views/pages/Login.vue
@@ -7,9 +7,12 @@
             <h1 class=" display-3 me-4 my-center" >Přihlašte se prosím vaším Google účtem</h1>
 
           </div>
-          <RouterLink to="/dashboard">
-            <CButton color="primary" size="lg">Login</CButton>
-          </RouterLink>
+          <div>
+          <a :href="getGoogleUrl('/')" class="google-auth">
+            <img :src="GoogleLogo" alt="Google Logo" />
+            <span class="auth-span">Google Login</span>
+          </a>
+        </div>
         </CCol>
       </CRow>
     </CContainer>
@@ -19,10 +22,63 @@
 .my-center {
   text-align: center;
 }
+.auth-span {
+  text-align: center;
+  color: black;
+  margin-left: 20px;
+  font-size: x-large;
+  text-decoration: none;
+}
+
+.auth-span:link {
+  text-decoration: none;
+}
+
+.auth-span:visited {
+  text-decoration: none;
+}
+
+.auth-span:hover {
+  text-decoration: none;
+}
+
+.auth-span:active {
+  text-decoration: none;
+}
+
+.google-auth {
+  background-color: #fff;
+  border-radius: 1px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.3s ease-in-out;
+  margin-top: 20px;
+ }
+
+.google-auth img {
+  height: 4rem;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.google-auth:hover {
+  box-shadow: 0 1px 13px 0 rgb(0 0 0 / 15%);
+}
 </style>
 
 <script>
+import GoogleLogo from '/src/assets/images/google.svg';
+import { getGoogleUrl } from '/src/utils/getGoogleUrl';
+
 export default {
   name: 'Login',
+  setup() {
+    return {
+      GoogleLogo,
+      getGoogleUrl
+    }
+  }
 }
 </script>
