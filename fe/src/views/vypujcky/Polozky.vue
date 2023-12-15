827784d4c6810033986eb14011357626348a2a21
oren.holis <oren@ilico.io>
2023-12-15 10:22:35 +0100

    Adding items table

--- fe/src/views/vypujcky/Polozky.vue
+++ fe/src/views/vypujcky/Polozky.vue
@@ -1,61 +1,71 @@
 <template>
   <div>
-    <CRow>
-      <CCol :md="12">
-        <CCard class="mb-4">
-          <CCardBody>
-            <CRow>
-              <CCol :sm="5">
-                <h4 id="traffic" class="card-title mb-0">Polozky
+    <CTable :columns="data.columns" :items="data.items" hower striped />
 
-                </h4>
-                <div class="small text-body-secondary">January 2021</div>
-              </CCol>
-              <CCol :sm="7" class="d-none d-md-block">
-                <CButton color="primary" class="float-end">
-                  <CIcon icon="cil-cloud-download" />
-                </CButton>
-              </CCol>
-            </CRow>
-            <br>
-            <CRow>
-              <CCol><p>ahhoj</p></CCol>
-            </CRow>
-          </CCardBody>
-          <CCardFooter>
-            <CRow>
-              <CCol>
-                <div class="text-body-secondary">Test</div>
-                <div class="fw-semibold text-truncate">29.703 Users (40%)</div>
-                <CProgress class="mt-2" color="success" thin :precision="1" :value="40" />
-              </CCol>
-            </CRow>
-          </CCardFooter>
-        </CCard>
-      </CCol>
-    </CRow>
-    <CRow>
-      <CCol :md="12">
-        <CCard class="mb-4">
-          <CCardHeader> Traffic &amp; Sales </CCardHeader>
-          <CCardBody>
-            Body
-          </CCardBody>
-        </CCard>
-      </CCol>
-    </CRow>
   </div>
 </template>
 
 <script>
+
+import {Formats} from "../../utils/utils";
+import { ref } from "vue";
 
 export default {
   name: 'Dashboard',
   components: {
   },
   setup() {
-    
+    let data = ref(
+      {
+        columns: [
+          {
+            key: 'code',
+            label: 'Kód položky',
+            _props: { scope: 'col' },
+          },
+          {
+            key: 'name',
+            label: 'Název položky',
+            _props: { scope: 'col' },
+          },
+          {
+            key: 'description',
+            label: 'Popis',
+            _props: { scope: 'col' },
+          },
+          {
+            key: 'room',
+            label: 'Místnost',
+            _props: { scope: 'col' },
+          },
+          {
+            key: 'added',
+            label: 'Přidáno do inventáře',
+            _props: { scope: 'col' }
+          },
+          {
+            key: 'serial_number',
+            label: 'Výrobní číslo',
+            _props: { scope: 'col' },
+          }
+        ],
+        items: [
+          {
+            code: 'DHM-2002',
+            room: '111',
+            added: '2011-10-05T14:48:00.000Z',
+            name: 'Name',
+            description: 'yes',
+            serial_number: 'yes',
+          },
+        ].map(i => ({
+          ...i,
+          added: Formats.date((new Date(i.added)).getTime() / 1000)
+        }))
+      }
+    )
     return {
+      data
     }
   },
 }
