d885cd2858b7fcbc0e675ad84cd6f273df13af40
Voplakal <82812912+Voplakal@users.noreply.github.com>
2023-12-15 10:35:19 +0100

    .

--- fe/src/views/vypujcky/VypujckyTable.vue
+++ fe/src/views/vypujcky/VypujckyTable.vue
@@ -1,6 +1,55 @@
 <template>
   <div>
-    <CTable :columns="data.columns" :items="data.items" hower striped />
+    <!-- <CTable :columns="columns" :items="items" hower striped /> -->
+    <CTable class="mb-0 border" hover striped responsive>
+      <CTableHead class="text-nowrap">
+        <CTableRow>
+          <CTableHeaderCell class="bg-body-secondary"> Kód </CTableHeaderCell>
+          <CTableHeaderCell class="bg-body-secondary text-center"> Člověk </CTableHeaderCell>
+          <CTableHeaderCell class="bg-body-secondary text-center"> Od </CTableHeaderCell>
+          <CTableHeaderCell class="bg-body-secondary"> Do </CTableHeaderCell>
+          <CTableHeaderCell class="bg-body-secondary"> Nazev </CTableHeaderCell>
+          <CTableHeaderCell class="bg-body-secondary"> Prodlouženo </CTableHeaderCell>
+          <CTableHeaderCell class="bg-body-secondary"> Poznámka </CTableHeaderCell>
+          <CTableHeaderCell class="bg-body-secondary" v-if="actions"> Akce </CTableHeaderCell>
+        </CTableRow>
+      </CTableHead>
+      <CTableBody>
+        <CTableRow v-for="item in items" :key="item.code">
+          <CTableDataCell class="text-center">
+            <CAvatar size="md" :src="item.avatar.src" :status="item.avatar.status" />
+          </CTableDataCell>
+          <CTableDataCell>
+            <div>{{ item.user.name }}</div>
+            <div class="small text-body-secondary text-nowrap">
+              <span>{{ item.user.new ? 'New' : 'Recurring' }}</span> |
+              {{ item.user.registered }}
+            </div>
+          </CTableDataCell>
+          <CTableDataCell class="text-center">
+            <CIcon size="xl" :name="item.country.flag" :title="item.country.name" />
+          </CTableDataCell>
+          <CTableDataCell>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="fw-semibold">{{ item.usage.value }}%</div>
+              <div class="text-nowrap text-body-secondary small ms-3">
+                {{ item.usage.period }}
+              </div>
+            </div>
+            <CProgress thin :color="item.usage.color" :value="item.usage.value" />
+          </CTableDataCell>
+          <CTableDataCell class="text-center">
+            <CIcon size="xl" :name="item.payment.icon" />
+          </CTableDataCell>
+          <CTableDataCell>
+            <div class="small text-body-secondary">Last login</div>
+            <div class="fw-semibold text-nowrap">
+              {{ item.activity }}
+            </div>
+          </CTableDataCell>
+        </CTableRow>
+      </CTableBody>
+    </CTable>
 
   </div>
 </template>
@@ -12,82 +61,41 @@ export default {
   components: {
   },
   setup() {
-    //kód, clovek,od,do, nazev polozky, poznámka, prodlouženo, tkacitka: detail, vrátit, prodlouzit  
-    let data = ref(
-      {
-        columns: [
-          {
-            key: 'code',
-            label: 'Kód položky',
-            _props: { scope: 'col' },
-          },
-          {
-            key: 'student',
-            label: 'Student',
-            _props: { scope: 'col' },
-          },
-          {
-            key: 'from',
-            label: 'Od',
-            _props: { scope: 'col' },
-          },
-          {
-            key: 'to',
-            label: 'Do',
-            _props: { scope: 'col' },
-          },
-          {
-            key: 'name',
-            label: 'Název položky',
-            _props: { scope: 'col' },
-          },
-          {
-            key: 'note',
-            label: 'Poznámka',
-            _props: { scope: 'col' },
-          },
-          {
-            key: 'prolonged',
-            label: 'Prodlouženo',
-            _props: { scope: 'col' },
-          },
-          //buttons
-          {
-            key: 'detail',
-            label: '',
-            _props: { scope: 'col' },
-          },
-          {
-            key: 'return',
-            label: '',
-            _props: { scope: 'col' },
-          },
-          {
-            key: 'longer',
-            label: '',
-            _props: { scope: 'col' },
-          },
-        ],
-        items: [
-          {
-            code: 'DHM-2002',
-            student: 'Mark Mark (mart@sd.cz)',
-            from: '3.3.2000',
-            to: '3.5.5000',
-            name: 'Name',
-            note: 'Poznamkskkaksk',
-            prolonged: 'yes',
-            //buttons enabled
-            detail: true,
-            return: true,
-            longer: true,
-          },
-          
-        ]
-      }
-    )
+    //kód, clovek,od,do, nazev polozky, poznámka, prodlouženo, tkacitka: detail, vrátit, prodlouzit 
+    
+
+    //first_name, last_name, email, i.name, i.code
+
+    let items =
+      [
+        {
+          id: "1234",
+          item_code: 'DHM-2002',
+          first_name: 'Mark',
+          last_name: " Nopeeee",
+          email: 'mmm@asd.cz',
+          from: '3.3.2000',
+          to: '3.5.5000',
+          item_name: 'Name',
+          note: 'Poznamkskkaksk',
+          prolonged: true,
+        },
+
+      ].map(i => ({
+        ...i,
+        from: Formats.date((new Date(i.from)).getTime() / 1000),
+        to: Formats.date((new Date(i.to)).getTime() / 1000),
+        prolonged: prolonged ? "Ano" : "Ne",
+        //buttons enabled
+        detail: true,
+        return: true,
+        longer: true,
+      }))
+
+
     return {
-      data
+      items,
+      actions
     }
   },
 }
